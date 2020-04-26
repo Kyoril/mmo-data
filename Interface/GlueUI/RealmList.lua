72c58e73b9d829cdd938433be11ad3d1ae82082b
Robin Klimonow <r.klimonow@chaoscore.net>
2020-04-26 10:20:40 +0200

    Added realm list functionality

--- Interface/GlueUI/RealmList.lua
+++ Interface/GlueUI/RealmList.lua
@@ -3,6 +3,11 @@
 selectedRealm = nil			-- Selected realm name
 
 
+function RealmListItem_Clicked(item)
+	selectedRealm = item.userData
+	RealmListButton01:Enable()
+end
+
 function RealmList_Show()
 	RealmListFrame:Show()
 	RealmListButton01:Disable()
@@ -10,10 +15,41 @@ function RealmList_Show()
 	-- Clear realm list entries
 	RealmList:RemoveAllChildren()
 	
-	-- Load realm data
+	-- Reset selected realm
+	selectedRealm = nil
+	
+	-- Remember the last cloned realm list item as we need it as anchor 
+	-- reference frame to align the list items beneath each other
+	local lastRealmListItem = nil
+	
+	-- Load realm list data for iteration
 	local realms = loginConnector:GetRealms()
 	for realm in realms do
+		-- Clone item
+		local realmListItem = RealmListItem:Clone()
+		RealmList:AddChild(realmListItem)
+		
+		-- Assign realm data
+		realmListItem:SetText(realm.name)
+		realmListItem.userData = realm
+		
+		-- Setup anchor points
+		if (lastRealmListItem ~= nil) then
+			realmListItem:SetAnchor(AnchorPoint.TOP, AnchorPoint.BOTTOM, lastRealmListItem, 0.0)
+		else
+			realmListItem:SetAnchor(AnchorPoint.TOP, AnchorPoint.TOP, nil, 0.0)
+		end
+		realmListItem:SetAnchor(AnchorPoint.LEFT, AnchorPoint.LEFT, nil, 0.0)
+		realmListItem:SetAnchor(AnchorPoint.RIGHT, AnchorPoint.RIGHT, nil, 0.0)
+		
+		-- Register click handler
+		realmListItem:SetClickedHandler(RealmListItem_Clicked)
+		
+		-- Add realm to realm list frame
 		print("Realm: "..realm.id.." - "..realm.name)
+
+		-- Remember realm list item
+		lastRealmListItem = realmListItem
 	end	
 end
 
