6cc96eb9d11f84ecc3427ad1a3e0ae9d120fa9ff
Robin Klimonow <r.klimonow@gmail.com>
2022-01-23 12:15:24 +0100

    Fix textures Added game menu and game menu button Fixed model texture coordinates, axis and scaling

---
+++ Interface/GameUI/GameMenu.lua
@@ -0,0 +1,35 @@
+
+BUTTON_H_PADDING = 16.0
+BUTTON_V_PADDING = 16.0
+
+local menuOffsetY = BUTTON_V_PADDING
+
+function AddMenuButton(text, callback)
+	local childCount = GameMenu:GetChildCount()
+
+	-- Create a new button from the template and add it to the window
+	local button = GameMenuButtonTemplate:Clone()
+	button:SetText(Localize(text))
+	button:SetClickedHandler(callback)
+	GameMenuButtons:AddChild(button)
+
+	-- Setup anchor points
+	button:SetAnchor(AnchorPoint.TOP, AnchorPoint.TOP, nil, menuOffsetY)
+	button:SetAnchor(AnchorPoint.LEFT, AnchorPoint.LEFT, nil, BUTTON_H_PADDING)
+	button:SetAnchor(AnchorPoint.RIGHT, AnchorPoint.RIGHT, nil, -BUTTON_H_PADDING)
+
+	menuOffsetY = menuOffsetY + button:GetHeight() + BUTTON_V_PADDING
+
+	GameMenu:SetHeight(menuOffsetY + BUTTON_V_PADDING + 190.0)
+end
+
+function OnQuitButton_Clicked()
+	RunConsoleCommand("quit")
+end
+
+function OnCloseButton_Clicked()
+	GameMenu:Hide()
+end
+
+AddMenuButton("CLOSE", OnCloseButton_Clicked)
+AddMenuButton("QUIT", OnQuitButton_Clicked)
---
+++ Interface/GameUI/GameMenuBar.lua
@@ -0,0 +1,32 @@
+
+MENUBAR_H_PADDING = 16.0
+MENUBAR_V_PADDING = 16.0
+
+local menuBarOffset = MENUBAR_H_PADDING + 20.0
+
+function AddMenuBarButton(text, callback)
+	local childCount = GameMenu:GetChildCount()
+
+	-- Create a new button from the template and add it to the window
+	local button = GameMenuBarButtonTemplate:Clone()
+	button:SetText(Localize(text))
+	button:SetClickedHandler(callback)
+	GameMenuBar:AddChild(button)
+
+	-- Setup anchor points
+	button:SetAnchor(AnchorPoint.TOP, AnchorPoint.TOP, nil, MENUBAR_V_PADDING)
+	button:SetAnchor(AnchorPoint.LEFT, AnchorPoint.LEFT, nil, menuBarOffset)
+
+	-- Increase offset
+	menuBarOffset = menuBarOffset + button:GetWidth() + MENUBAR_H_PADDING
+end
+
+function OnMenuItem_Clicked()
+	if (GameMenu:IsVisible()) then
+		GameMenu:Hide()
+	else
+		GameMenu:Show()
+	end
+end
+
+AddMenuBarButton("MENU", OnMenuItem_Clicked)
