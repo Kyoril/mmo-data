002c255b3f80d4fde27c3e90bbc17ad08fed489d
Robin Klimonow <r.klimonow@gmail.com>
2023-10-30 21:39:56 +0100

    Register for chat events

--- Interface/GameUI/ChatFrame.lua
+++ Interface/GameUI/ChatFrame.lua
@@ -1,3 +1,9 @@
 
--- Hello!
-print("Chat frame lua loaded!")
+function ChatFrame_OnLoad(this)
+    this:RegisterEvent("CHAT_MSG_SAY", function(this, character, message)
+        ChatFrame:AddMessage("[" .. string.format("%x", character) .. "] says: " .. message, 1.0, 1.0, 1.0)
+    end)
+    this:RegisterEvent("CHAT_MSG_YELL", function(this, character, message)
+        ChatFrame:AddMessage("[" .. string.format("%x", character) .. "] yells: " .. message, 1.0, 0.0, 0.0)
+    end)
+end
