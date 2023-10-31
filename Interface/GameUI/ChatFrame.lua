f1b4cf8c95e158ba756c6046d4aeab0f49958e3d
Robin Klimonow <robin.klimonow@quibiq.de>
2023-10-31 18:17:13 +0100

    UI updates with action bar

--- Interface/GameUI/ChatFrame.lua
+++ Interface/GameUI/ChatFrame.lua
@@ -6,4 +6,18 @@ function ChatFrame_OnLoad(this)
     this:RegisterEvent("CHAT_MSG_YELL", function(this, character, message)
         ChatFrame:AddMessage("[" .. string.format("%x", character) .. "] yells: " .. message, 1.0, 0.0, 0.0)
     end)
+
+    ChatFrame:AddMessage("Welcome to the game!", 1.0, 1.0, 0.0);
+end
+
+function ChatFrame_ScrollDown(this)
+    ChatFrame:ScrollDown();
+end
+
+function ChatFrame_ScrollUp(this)
+    ChatFrame:ScrollUp()
+end
+
+function ChatFrame_ScrollToBottom(this)
+    ChatFrame:ScrollToBottom()
 end
--- Interface/GameUI/GameMenuBar.lua
+++ Interface/GameUI/GameMenuBar.lua
@@ -1,8 +1,8 @@
 
-MENUBAR_H_PADDING = 8.0
+MENUBAR_H_PADDING = 0.0
 MENUBAR_V_PADDING = 4.0
 
-local menuBarOffset = MENUBAR_H_PADDING + 20.0
+local menuBarOffset = MENUBAR_H_PADDING + 0.0
 
 function AddMenuBarButton(text, callback)
 	local childCount = GameMenu:GetChildCount()
@@ -10,18 +10,16 @@ function AddMenuBarButton(text, callback)
 	-- Create a new button from the template and add it to the window
 	local button = GameMenuBarImageButton:Clone()
 	button:SetText(Localize(text))
+	button:SetProperty("Icon", text)
 	button:SetClickedHandler(callback)
-	GameMenuBar:AddChild(button)
+	MenuBarButtons:AddChild(button)
 
 	-- Setup anchor points
-	button:SetAnchor(AnchorPoint.TOP, AnchorPoint.TOP, nil, MENUBAR_V_PADDING)
 	button:SetAnchor(AnchorPoint.BOTTOM, AnchorPoint.BOTTOM, nil, -MENUBAR_V_PADDING)
 	button:SetAnchor(AnchorPoint.LEFT, AnchorPoint.LEFT, nil, menuBarOffset)
 
 	-- Increase offset
 	menuBarOffset = menuBarOffset + button:GetWidth() + MENUBAR_H_PADDING
-	print("Added menu button " .. text)
-
 end
 
 function OnMenuItem_Clicked()
@@ -36,5 +34,5 @@ function OnCharacter_Clicked()
 
 end
 
-AddMenuBarButton("MENU", OnMenuItem_Clicked)
-AddMenuBarButton("CHARACTER", OnCharacter_Clicked)
+AddMenuBarButton("Interface/Icons/fg4_icons_menu_result.htex", OnMenuItem_Clicked)
+AddMenuBarButton("Interface/Icons/fg4_icons_helmet_result.htex", OnCharacter_Clicked)
