73184380121c2b46107ad23ec45c62f14bda5a4e
Robin Klimonow <r.klimonow@gmail.com>
2024-11-25 20:37:13 +0100

    Added sword material Vendor frame handles some vendor events now

--- Interface/GameUI/MerchantFrame.lua
+++ Interface/GameUI/MerchantFrame.lua
@@ -1,8 +1,21 @@
+MERCHANT_ITEMS_PER_PAGE = 14;
+
+function MerchantFrame_Show(self)
+    self:GetChild(0):SetText(UnitName("target"));
+    ShowUIPanel(self);
+end
+
+function MerchantFrame_Close(self)
+    HideUIPanel(self);
+end
 
 function MerchantFrame_OnLoad(self)
+    -- Initialize side panel functionality first, like the close button
+    SidePanel_OnLoad(self);
 
+    self:RegisterEvent("VENDOR_SHOW", MerchantFrame_Show);
+    self:RegisterEvent("VENDOR_CLOSED", MerchantFrame_Close);
 end
 
 function MerchantFrame_OnShow(self)
-
 end
