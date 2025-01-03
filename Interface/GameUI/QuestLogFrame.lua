dc3bf0c1a8a810abee4cf05743c5555cf6501958
Robin Klimonow <r.klimonow@gmail.com>
2025-01-03 11:58:01 +0100

    Questlog frame is now updating correctly on quest log update events Showing the quest log again will show the updated currently selected quest correctly (if any)

--- Interface/GameUI/QuestLogFrame.lua
+++ Interface/GameUI/QuestLogFrame.lua
@@ -1,5 +1,6 @@
 
 MAX_DISPLAY_QUESTS = 6;
+MAX_QUESTLOG_SIZE = 20;
 
 local selectedQuestId = nil;
 
@@ -14,7 +15,7 @@ function QuestLogFrame_OnLoad(self)
     QuestLogAbandonButton:Disable();
 
     -- Register for events
-    self:RegisterEvent("QUEST_LOG_UPDATE", QuestLog_Update);
+    self:RegisterEvent("QUEST_LOG_UPDATE", QuestLogFrame_OnUpdateQuestLog);
 
     -- Add button to the main menu bar of the game
     AddMenuBarButton("Interface/Icons/fg4_icons_questlog_result.htex", QuestLogFrame_Toggle);
@@ -37,6 +38,60 @@ end
 
 function QuestLogFrame_OnShow(self)
     QuestLog_Update();
+
+    if GetQuestLogSelection() ~= 0 then
+        QuestLogFrame_UpdateQuestDetails();
+    end
+end
+
+function QuestLogFrame_UpdateQuestDetails()
+    local selectedQuestId = GetQuestLogSelection();
+    local questLogEntry = nil;
+
+    for i = 1, MAX_QUESTLOG_SIZE do
+        local entry = GetQuestLogEntry(i - 1);
+        if entry and entry.quest.id == selectedQuestId then
+            questLogEntry = entry;
+            break;
+        end
+    end
+
+    if not questLogEntry then
+        QuestLogQuestDetailScrollContent:Hide();
+        return;
+    end
+
+    QuestLogQuestDetailTitle:SetText(questLogEntry.quest.title);
+    QuestLogQuestDetailDetails:SetText(GetQuestDetailsText(questLogEntry.quest));
+    QuestLogQuestDetailDetails:SetHeight(QuestLogQuestDetailDetails:GetTextHeight());
+
+    QuestLogObjectiveList:RemoveAllChildren();
+
+    local count = GetQuestObjectiveCount();
+    for i = 1, count do
+        local objective = GetQuestObjectiveText(i - 1);
+        
+        local label = TextSmall:Clone();
+        label:SetProperty("TextColor", "FF100500");
+        label:SetProperty("Font", "QuestFont");
+        label:SetAnchor(AnchorPoint.LEFT, AnchorPoint.LEFT, nil, 0);
+        label:SetAnchor(AnchorPoint.RIGHT, AnchorPoint.RIGHT, nil, 0);
+        label:SetAnchor(AnchorPoint.TOP, AnchorPoint.TOP, nil, (i - 1) * 32);
+        if objective then
+            label:SetText(objective);
+        else
+            label:SetText("UNKNOWN");
+        end
+        QuestLogObjectiveList:AddChild(label);
+    end
+    QuestLogObjectiveList:SetHeight(count * 32);
+    
+    QuestLogQuestDetailObjectivesHeader:SetAnchor(AnchorPoint.TOP, AnchorPoint.BOTTOM, QuestLogObjectiveList, 32);
+    QuestLogQuestDetailObjectives:SetText(GetQuestObjectivesText(questLogEntry.quest));
+    QuestLogQuestDetailObjectives:SetHeight(QuestLogQuestDetailObjectives:GetTextHeight());
+    QuestLogQuestDetailRewards:SetAnchor(AnchorPoint.TOP, AnchorPoint.BOTTOM, QuestLogQuestDetailObjectives, 32);
+    QuestLogQuestDetailScrollContent:Show();
+    QuestLogAbandonButton:Enable();
 end
 
 function QuestListQuestButton_OnClick(self)
@@ -56,39 +111,15 @@ function QuestListQuestButton_OnClick(self)
         selectedQuestId = self.userData.quest.id;
         QuestLogSelectQuest(selectedQuestId);
 
-        QuestLogQuestDetailTitle:SetText(self.userData.quest.title);
-        QuestLogQuestDetailDetails:SetText(GetQuestDetailsText(self.userData.quest));
-        QuestLogQuestDetailDetails:SetHeight(QuestLogQuestDetailDetails:GetTextHeight());
+        QuestLogFrame_UpdateQuestDetails();
+    end
+end
 
-        QuestLogObjectiveList:RemoveAllChildren();
+function QuestLogFrame_OnUpdateQuestLog(self)
+    QuestLog_Update();
 
-        local count = GetQuestObjectiveCount();
-        for i = 1, count do
-            local objective = GetQuestObjectiveText(i - 1);
-            
-            local label = TextSmall:Clone();
-            label:SetProperty("TextColor", "FF100500");
-            label:SetProperty("Font", "QuestFont");
-            label:SetAnchor(AnchorPoint.LEFT, AnchorPoint.LEFT, nil, 0);
-            label:SetAnchor(AnchorPoint.RIGHT, AnchorPoint.RIGHT, nil, 0);
-            label:SetAnchor(AnchorPoint.TOP, AnchorPoint.TOP, nil, (i - 1) * 32);
-            if objective then
-                label:SetText(objective);
-            else
-                label:SetText("UNKNOWN");
-            end
-            QuestLogObjectiveList:AddChild(label);
-        end
-        QuestLogObjectiveList:SetHeight(count * 32);
-        
-        QuestLogQuestDetailObjectivesHeader:SetAnchor(AnchorPoint.TOP, AnchorPoint.BOTTOM, QuestLogObjectiveList, 32);
-        QuestLogQuestDetailObjectives:SetText(GetQuestObjectivesText(self.userData.quest));
-        QuestLogQuestDetailObjectives:SetHeight(QuestLogQuestDetailObjectives:GetTextHeight());
-        QuestLogQuestDetailRewards:SetAnchor(AnchorPoint.TOP, AnchorPoint.BOTTOM, QuestLogQuestDetailObjectives, 32);
-
-        QuestLogQuestDetailScrollContent:Show();
-        
-        QuestLogAbandonButton:Enable();
+    if GetQuestLogSelection() ~= 0 then
+        QuestLogFrame_UpdateQuestDetails();
     end
 end
 
@@ -109,6 +140,7 @@ function QuestLog_Update()
                 button:SetText(questLogEntry.quest.title);
             end
 
+            button:SetChecked(questLogEntry.quest ~= nil and questLogEntry.quest.id == GetQuestLogSelection());
             button:Show();
         else
             button.userData = nil;
