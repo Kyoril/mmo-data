0bba12f9bb45c46d28f8d9c6e2a66455e3ea7858
Robin Klimonow <r.klimonow@gmail.com>
2025-04-13 15:51:29 +0200

    Updated brick material to include NormalPower parameter Updated sky material to fade in with fog color Updated wireframe editor material to fix wireframe-on-terrain rendering

--- PSForward.hlsl
+++ PSForward.hlsl
@@ -25,16 +25,19 @@ cbuffer CameraParameters : register(b1)
 cbuffer ScalarParameters : register(b2)
 {
 	float sScaleUV;
-	float sEdgeContrast;
-	float sEdgePower;
-	float sRoughnessEdge;
-	float sRoughness;
+	float sScaleMaskDirt;
+	float sIntensityMaskDirt;
+	float sDirtContrast;
+	float sDirtPower;
+	float sNormalPower;
+	float sRougness;
 };
 
 cbuffer VectorParameters : register(b3)
 {
-	float4 vEdgeColor;
+	float4 vDirtColor;
 	float4 vHueColor;
+	float4 vHueMaskDirt;
 };
 
 struct VertexOut
@@ -50,17 +53,17 @@ struct VertexOut
 	float3 viewPos : TEXCOORD3;
 };
 
+// Textures/T_plaster_Mask.htex
+Texture2D tex0;
+SamplerState sampler0;
+
 // BaseColor
 Texture2D texparam0;
 SamplerState paramsampler0;
 
-// BuildingNormal
+// Normal
 Texture2D texparam1;
 SamplerState paramsampler1;
-
-// Normal
-Texture2D texparam2;
-SamplerState paramsampler2;
 
 float3 GetWorldNormal(float3 tangentSpaceNormal, float3 N, float3 T, float3 B)
 {
@@ -119,7 +122,7 @@ float4 main(VertexOut input) : SV_Target
 	float3 B = normalize(input.binormal);
 	float3 T = normalize(input.tangent);
 	float3x3 TBN = float3x3(T, B, N);
-	float4 expr_0 = vEdgeColor;
+	float4 expr_0 = vDirtColor;
 
 	float3 expr_1 = expr_0.rgb;
 
@@ -195,60 +198,196 @@ float4 main(VertexOut input) : SV_Target
 
 	float3 expr_37 = float3(expr_31, expr_36);
 
-	float expr_38 = 0.000000;
+	float expr_38 = expr_37.r;
 
-	float expr_39 = sEdgeContrast;
+	float expr_39 = 0.500000;
 
-	float expr_40 = expr_38 - expr_39;
+	float3 expr_40 = 1.0 - expr_37;
 
-	float expr_41 = 1.000000;
+	float expr_41 = 2.000000;
 
-	float expr_42 = expr_39 + expr_41;
+	float3 expr_42 = expr_40 * expr_41;
 
-	float2 expr_43 = input.uv0;
+	float4 expr_43 = vHueMaskDirt;
 
-	float4 expr_44 = (texparam1.Sample(paramsampler1, expr_43.xy) * 2.0 - 1.0);
+	float3 expr_44 = expr_43.rgb;
 
-	float expr_45 = expr_44.b;
+	float3 expr_45 = 1.0 - expr_44;
 
-	float expr_46 = sEdgePower;
+	float3 expr_46 = expr_42 * expr_45;
 
-	float expr_47 = pow(expr_45, expr_46);
+	float3 expr_47 = 1.0 - expr_46;
 
-	float expr_48 = lerp(expr_40, expr_42, expr_47);
+	float expr_48 = expr_47.r;
 
-	float expr_49 = 0.000000;
+	float expr_49 = 2.000000;
 
-	float expr_50 = 1.000000;
+	float3 expr_50 = expr_37 * expr_49;
 
-	float expr_51 = clamp(expr_48, expr_49, expr_50);
+	float3 expr_51 = expr_43.rgb;
 
-	float3 expr_52 = lerp(expr_1, expr_37, expr_51);
+	float3 expr_52 = expr_50 * expr_51;
 
-	float3 expr_53 = expr_44.rgb;
+	float expr_53 = expr_52.r;
 
-	float4 expr_54 = (texparam2.Sample(paramsampler2, expr_4.xy) * 2.0 - 1.0);
+	float expr_54 = select((expr_38 >= expr_39), expr_48, expr_53);
 
-	float3 expr_55 = expr_54.rgb;
+	float expr_55 = expr_37.g;
 
-	float3 expr_56 = lerp(expr_53, expr_55, expr_51);
+	float expr_56 = expr_47.g;
 
-	float expr_57 = sRoughnessEdge;
+	float expr_57 = expr_52.g;
 
-	float expr_58 = expr_5.a;
+	float expr_58 = select((expr_55 >= expr_39), expr_56, expr_57);
 
-	float expr_59 = sRoughness;
+	float2 expr_59 = float2(expr_54, expr_58);
 
-	float expr_60 = expr_58 * expr_59;
+	float expr_60 = expr_37.b;
 
-	float expr_61 = lerp(expr_57, expr_60, expr_51);
+	float expr_61 = expr_47.b;
 
-	N = expr_56;
+	float expr_62 = expr_52.b;
+
+	float expr_63 = select((expr_60 >= expr_39), expr_61, expr_62);
+
+	float3 expr_64 = float3(expr_59, expr_63);
+
+	float3 expr_65 = input.worldPos.xyz;
+
+	float expr_66 = sScaleMaskDirt;
+
+	float expr_67 = abs(expr_66);
+
+	float expr_68 = -1.000000;
+
+	float expr_69 = expr_67 * expr_68;
+
+	float3 expr_70 = expr_65 / expr_69;
+
+	float2 expr_71 = expr_70.rb;
+
+	float4 expr_72 = tex0.Sample(sampler0, expr_71.xy);
+
+	float3 expr_73 = expr_72.rgb;
+
+	float2 expr_74 = expr_70.gb;
+
+	float4 expr_75 = tex0.Sample(sampler0, expr_74.xy);
+
+	float3 expr_76 = expr_75.rgb;
+
+	float expr_77 = 0.000000;
+
+	float expr_78 = 1.000000;
+
+	float expr_79 = expr_77 - expr_78;
+
+	float expr_80 = 1.000000;
+
+	float expr_81 = expr_78 + expr_80;
+
+	float3 expr_82 = N;
+
+	float expr_83 = expr_82.r;
+
+	float expr_84 = abs(expr_83);
+
+	float expr_85 = lerp(expr_79, expr_81, expr_84);
+
+	float expr_86 = 0.000000;
+
+	float expr_87 = 1.000000;
+
+	float expr_88 = clamp(expr_85, expr_86, expr_87);
+
+	float expr_89 = expr_88.r;
+
+	float3 expr_90 = lerp(expr_73, expr_76, expr_89);
+
+	float2 expr_91 = expr_70.rg;
+
+	float4 expr_92 = tex0.Sample(sampler0, expr_91.xy);
+
+	float3 expr_93 = expr_92.rgb;
+
+	float expr_94 = expr_82.b;
+
+	float expr_95 = abs(expr_94);
+
+	float expr_96 = lerp(expr_79, expr_81, expr_95);
+
+	float expr_97 = 0.000000;
+
+	float expr_98 = 1.000000;
+
+	float expr_99 = clamp(expr_96, expr_97, expr_98);
+
+	float expr_100 = expr_99.r;
+
+	float3 expr_101 = lerp(expr_90, expr_93, expr_100);
+
+	float expr_102 = expr_101.r;
+
+	float3 expr_103 = lerp(expr_64, expr_37, expr_102);
+
+	float expr_104 = sIntensityMaskDirt;
+
+	float3 expr_105 = lerp(expr_37, expr_103, expr_104);
+
+	float expr_106 = 0.000000;
+
+	float expr_107 = sDirtContrast;
+
+	float expr_108 = expr_106 - expr_107;
+
+	float expr_109 = 1.000000;
+
+	float expr_110 = expr_107 + expr_109;
+
+	float4 expr_111 = (texparam1.Sample(paramsampler1, expr_4.xy) * 2.0 - 1.0);
+
+	float expr_112 = expr_111.b;
+
+	float expr_113 = sDirtPower;
+
+	float expr_114 = pow(expr_112, expr_113);
+
+	float expr_115 = lerp(expr_108, expr_110, expr_114);
+
+	float expr_116 = 0.000000;
+
+	float expr_117 = 1.000000;
+
+	float expr_118 = clamp(expr_115, expr_116, expr_117);
+
+	float expr_119 = expr_118.r;
+
+	float3 expr_120 = lerp(expr_1, expr_105, expr_119);
+
+	float3 expr_121 = expr_111.rgb;
+
+	float3 expr_122 = expr_121.rgb;
+
+	float4 expr_123 = float4(0, 0, 1, 1);
+
+	float3 expr_124 = expr_123.rgb;
+
+	float expr_125 = sNormalPower;
+
+	float3 expr_126 = lerp(expr_122, expr_124, expr_125);
+
+	float expr_127 = expr_5.a;
+
+	float expr_128 = sRougness;
+
+	float expr_129 = expr_127 * expr_128;
+
+	N = expr_126;
 
 	N = GetWorldNormal(N, input.normal, input.tangent, input.binormal);
 	float specular = 0.5;
 
-	float roughness = saturate(expr_61);
+	float roughness = saturate(expr_129);
 
 	float metallic = 0.0;
 
@@ -256,7 +395,7 @@ float4 main(VertexOut input) : SV_Target
 
 	float3 baseColor = float3(1.0, 1.0, 1.0);
 
-	baseColor = expr_52;
+	baseColor = expr_120;
 
 	if (opacity <= 0.333) discard;
 	baseColor = pow(baseColor, 2.2);
