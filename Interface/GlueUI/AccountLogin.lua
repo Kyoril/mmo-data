3879c770ac80e5a5a78e4bacec4b86fba3a00796
Robin Klimonow <r.klimonow@gmail.com>
2025-04-25 19:56:11 +0200

    Fixed french localization error

--- Interface/GlueUI/AccountLogin.lua
+++ Interface/GlueUI/AccountLogin.lua
@@ -7,38 +7,38 @@
 function AccountLogin_Login()
 	-- Copy the text values so we don't care if somehow the control text is 
 	-- changed after this command
-	local username = AccountNameField:GetText()
-	local password = AccountPasswordField:GetText()
+	local username = AccountNameField:GetText();
+	local password = AccountPasswordField:GetText();
 
 	-- Disbale the login button
-	LoginButton:Disable()
+	LoginButton:Disable();
 	
 	-- Update the dialog label and show the dialog
-	GlueDialog_Show("CONNECTING")
+	GlueDialog_Show("CONNECTING");
 	
 	-- Do the login
-	RunConsoleCommand("login " .. username .. " " .. password)
+	RunConsoleCommand("login " .. username .. " " .. password);
 end
 
 -- Called when the realm list was received
 function AccountLogin_OnRealmList()
-	GlueDialog_Hide()
-	AccountLogin:Hide()
-	RealmList_Show()
+	GlueDialog_Hide();
+	AccountLogin:Hide();
+	RealmList_Show();
 end
 
 function AccountLogin_OnCharList()
-	GlueDialog_Hide()
-	CharCreate:Hide()
-	CharList_Show()
+	GlueDialog_Hide();
+	CharCreate:Hide();
+	CharList_Show();
 end
 
 function AccountLogin_AuthError(frame, errorCode)
-	GlueDialog_Show("AUTH_ERROR", AUTH_ERROR_STRING[errorCode])
+	GlueDialog_Show("AUTH_ERROR", AUTH_ERROR_STRING[errorCode]);
 end
 
 function AccountLogin_OnConnect()
-	GlueDialog_Show("CONNECTING")
+	GlueDialog_Show("CONNECTING");
 end
 
 -- This function is called when the AccountLogin frame is loaded
@@ -46,18 +46,18 @@ function AccountLogin_OnLoad()
 	-- Register the frame to receive events
 	AccountLogin:RegisterEvent("AUTH_SUCCESS", function()
 		GlueDialog_Show("RETRIEVE_REALM_LIST");
-	end)
-	AccountLogin:RegisterEvent("AUTH_FAILED", AccountLogin_AuthError)
+	end);
+	AccountLogin:RegisterEvent("AUTH_FAILED", AccountLogin_AuthError);
 	
 	AccountLogin:RegisterEvent("REALM_AUTH_SUCCESS", function()
 		GlueDialog_Show("RETRIEVE_CHAR_LIST");
-	end)
+	end);
 	AccountLogin:RegisterEvent("REALM_AUTH_FAILED", function(errorCode)
 		GlueDialog_Show("REALM_AUTH_ERROR", AUTH_ERROR_STRING[errorCode]);
-	end)
+	end);
 	AccountLogin:RegisterEvent("REALM_DISCONNECTED", function(errorCode)
 		GlueDialog_Show("REALM_AUTH_ERROR", "DISCONNECTED");
-	end)
+	end);
 	
 	-- Register realm list event
 	AccountLogin:RegisterEvent("REALM_LIST", AccountLogin_OnRealmList);
