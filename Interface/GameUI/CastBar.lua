5b1bf11691976032af6b3abf9e64a184221b5aaf
Robin Klimonow <r.klimonow@gmail.com>
2026-02-23 18:42:30 +0100

    Enhance cast bar functionality: Add channeling support and improve progress updates

--- Interface/GameUI/CastBar.lua
+++ Interface/GameUI/CastBar.lua
@@ -2,10 +2,12 @@
 CastBarSpell = nil;
 CastBarProgress = 0.0;
 CastBarEndTime = 1.0;
+CastBarIsChanneling = false;
 
 function CastBar_OnCastStart(self, spell, castTime)
     CastBarSpell = spell;
     CastBarProgress = 0.0;
+    CastBarIsChanneling = false;
 
     if (spell == nil) then
         return
@@ -29,6 +31,33 @@ end
 
 function CastBar_OnCastEnd(self, success)
     PlayerCastBar:Hide();
+    CastBarIsChanneling = false;
+end
+
+function CastBar_OnChannelStart(self, spell, duration)
+    CastBarSpell = spell;
+    CastBarProgress = 0.0;
+    CastBarIsChanneling = true;
+
+    if (spell == nil) then
+        return
+    end
+
+    if (duration == nil or duration <= 0) then
+        return
+    end
+
+    CastBarEndTime = duration / 1000.0;
+    PlayerCastBar:SetProperty("Progress", "1.0");
+    PlayerCastBar:SetText(CastBarSpell.name);
+
+    PlayerCastBar:Show();
+end
+
+function CastBar_OnChannelStop(self)
+    PlayerCastBar:Hide();
+    CastBarIsChanneling = false;
+    CastBarSpell = nil;
 end
 
 function CastBar_OnUpdate(self, elapsed)
@@ -38,12 +67,22 @@ function CastBar_OnUpdate(self, elapsed)
 
     CastBarProgress = CastBarProgress + elapsed;
 
-    -- Update progress value
-    PlayerCastBar:SetProperty("Progress", string.format("%.2f", CastBarProgress / CastBarEndTime));
+    if (CastBarIsChanneling) then
+        -- Channel: progress goes from 1.0 down to 0.0
+        local remaining = 1.0 - (CastBarProgress / CastBarEndTime);
+        if (remaining < 0.0) then
+            remaining = 0.0;
+        end
+        PlayerCastBar:SetProperty("Progress", string.format("%.2f", remaining));
+    else
+        -- Normal cast: progress goes from 0.0 up to 1.0
+        PlayerCastBar:SetProperty("Progress", string.format("%.2f", CastBarProgress / CastBarEndTime));
+    end
 end
 
 function CastBar_OnLoad(self)
     self:RegisterEvent("PLAYER_SPELL_CAST_START", CastBar_OnCastStart)
     self:RegisterEvent("PLAYER_SPELL_CAST_FINISH", CastBar_OnCastEnd)
+    self:RegisterEvent("PLAYER_CHANNEL_START", CastBar_OnChannelStart)
+    self:RegisterEvent("PLAYER_CHANNEL_STOP", CastBar_OnChannelStop)
 end
-
